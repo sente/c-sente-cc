d6f66ab075d01b9cf627750c9c0c13bbadcdf84f
Stuart Powers <stuart.powers@gmail.com>
2012-08-23 23:17:40 -0400

    added curls/426R/config_sample.js

---
+++ curls/426R/config_sample.js
@@ -0,0 +1,37 @@
+module.exports.TwilioCredentials = {
+    sid: 'xxxxxxxxxxxxxxxxxxxxxxxxxb5188bd0a',
+    authToken: 'xxxxxxxxxxxxx9d14a214ceeda1bd04f',
+    incoming: '+xxxxxxxxx69',
+    outgoing: '+xxxxxxxxx69',
+    hostname: 'xxxxxxxxxxxproto.com',
+    port: 3002
+};
+
+module.exports.DbCredentials = {
+	host:     'xxxxxxxxxxxxxxxxxxxxxzcvvicowff.us-east-1.rds.amazonaws.com', 
+	port:     '3306',
+	user:     'xxxxxxxxx',
+	password: 'xxxxxxxx',
+	database: 'xxxxxxxxatabase'	
+};
+
+module.exports.Auth = {
+    fb: {
+        appId: 'xxxxxxxxx578011'
+      , appSecret: 'xxxxxxxxxxxxxxx847c964ef70d41e9e'
+    }
+  , twit: {
+        consumerKey: 'xxxxxxxxxxxxrU98B2Zig'
+      , consumerSecret: 'xxxxxxxxxxxxxxxxxxNWeR1Dyk3JmWLKuxSxywvNOVU'
+    }
+  , googl: {
+	    key: 'xxxxxxxxxxxxYe1yKH88jtsQDP3SXxHF7vEu_uo'
+    }
+};
+
+module.exports.ApplicationHostURL = "http://xxxxxxxxx.cc:3001";
+module.exports.CdnUrl = 'http://xxxxxxxxxxx30.cloudfront.net/';
+module.exports.DefaultPackageID = 99;
+
+module.exports.Port = 3001;
+
